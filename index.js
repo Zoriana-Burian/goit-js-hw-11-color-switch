8e8ede65102c0994ec275ec90a55ac0e53d880f9
Zoriana-Burian <ke7sha1991@gmail.com>
2020-10-30 15:00:13 +0200

    додає методи старт, стоп

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const refs = {
+    btnStart: document.querySelector('button[data-action="start]'),
+    btnStop: document.querySelector('button[data-action="stop]')
+}
+
+
 const colors = [
     '#FFFFFF',
     '#2196F3',
@@ -10,3 +16,35 @@ const colors = [
   const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
+
+  refs.btnStart.addEventListener('click', () => {
+      timer.start();
+  })
+
+  refs.btnStop/addEventListener('click', () => {
+      timer.stop();
+  })
+
+  const timer = {
+      intervalId: null,
+      isActive: false,
+      start() {
+          
+        if(this.isActive){
+              return;
+          };
+
+          this.isActive = true;
+          const max = colors.length - 1;
+
+          this.intervalId = setInterval(() => {
+
+          }, 1000)
+          
+      },
+
+      stop() {
+          clearInterval(this.intervalId);
+          this.isActive = false;
+      }
+  }
